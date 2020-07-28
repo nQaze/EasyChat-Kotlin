b6c8134237fa0071336bffc5bf8b724461798b71
Nabil <nabilk@cdpindia.com>
2020-07-28 10:10:55 +0530

    Updated code as per CometChat SDK's best practices.

--- app/src/main/java/com/nqaze/easychat_kotlin/ContactsActivity.kt
+++ app/src/main/java/com/nqaze/easychat_kotlin/ContactsActivity.kt
@@ -3,10 +3,12 @@ package com.nqaze.easychat_kotlin
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.cometchat.pro.constants.CometChatConstants
 import com.cometchat.pro.models.User
 import constant.StringContract
 import listeners.OnItemClickListener
 import screen.CometChatUserListScreen
+import screen.messagelist.CometChatMessageListActivity
 
 class ContactsActivity : AppCompatActivity() {
 
@@ -16,10 +18,11 @@ class ContactsActivity : AppCompatActivity() {
 
         CometChatUserListScreen.setItemClickListener(object : OnItemClickListener<User?>() {
             override fun OnItemClick(user: User?, position: Int) {
-                val intent = Intent(this@ContactsActivity, ChatMessageActivity::class.java)
+                val intent = Intent(this@ContactsActivity, CometChatMessageListActivity::class.java)
                 intent.putExtra(StringContract.IntentStrings.UID, user?.uid)
                 intent.putExtra(StringContract.IntentStrings.NAME, user?.name)
                 intent.putExtra(StringContract.IntentStrings.AVATAR, user?.avatar)
+                intent.putExtra(StringContract.IntentStrings.TYPE, CometChatConstants.RECEIVER_TYPE_USER);
                 startActivity(intent)
             }
         })
--- app/src/main/java/com/nqaze/easychat_kotlin/ConversationsActivity.kt
+++ app/src/main/java/com/nqaze/easychat_kotlin/ConversationsActivity.kt
@@ -4,11 +4,13 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import com.cometchat.pro.constants.CometChatConstants
 import com.cometchat.pro.models.Conversation
 import com.cometchat.pro.models.User
 import constant.StringContract
 import listeners.OnItemClickListener
 import screen.CometChatConversationListScreen
+import screen.messagelist.CometChatMessageListActivity
 
 class ConversationsActivity : AppCompatActivity() {
 
@@ -18,11 +20,12 @@ class ConversationsActivity : AppCompatActivity() {
 
         CometChatConversationListScreen.setItemClickListener(object : OnItemClickListener<Conversation?>() {
             override fun OnItemClick(conversation: Conversation?, position: Int) {
-                val user = User.fromJson(conversation?.conversationWith.toString())
-                val intent = Intent(this@ConversationsActivity, ChatMessageActivity::class.java)
+                val user = conversation?.conversationWith as User
+                val intent = Intent(this@ConversationsActivity, CometChatMessageListActivity::class.java)
                 intent.putExtra(StringContract.IntentStrings.UID, user.uid)
                 intent.putExtra(StringContract.IntentStrings.NAME, user.name)
                 intent.putExtra(StringContract.IntentStrings.AVATAR, user.avatar)
+                intent.putExtra(StringContract.IntentStrings.TYPE, CometChatConstants.RECEIVER_TYPE_USER);
                 startActivity(intent)
             }
         })
